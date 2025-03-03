9d76438e03facde912e865aff98f471a7494f652
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-03-04 00:01:09 +0530

    .cpp

---
+++ decrementops.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=11;
+    int b=a--;    
+      cout<< b << endl ; // kaam then update so 11
+      cout << a << endl ; 
+  int c =10;
+  int d =--c;  //update then store -->>> pre increment 
+  cout<< d << endl;
+  cout << c << endl;
+    return 0;
+}
+/// similar for decrement ops ///
