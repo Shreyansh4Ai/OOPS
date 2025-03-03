4c0df5a8f43ab05ffd47fa76fc808f6536d96778
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-03-04 00:14:56 +0530

    .cpp

---
+++ calculatorlike.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a ,b;
+cout<<" enter the values for a and b"<<endl;
+cin>> a;
+cin>> b;
+// sum
+cout << "the sum is = "<< (a+b) << endl;
+//subtract 
+cout << "the difference is  is = "<< (a-b) << endl;
+//multiply
+cout << "the product  is = "<< (a*b) << endl ;
+//divide 
+cout << "the quotient  is = "<< (a/b) << endl;
+
+}
