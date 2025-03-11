2742d153fb6c9b185efd917f1cc102c4f1c0f826
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-03-11 11:02:57 +0530

    .py

---
+++ conditional statements/loops and control statements/nestedLoops.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m =10;
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<=n ; i++){
+          for(int j=0; j<=m;j++){
+            cout<< "*";
+
+          }  
+            cout<<endl;
+
+    }
+
+    return 0;
+}
