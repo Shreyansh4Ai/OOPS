57df30e9668285cb78c6e0144ae0425360d0432c
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-03-27 19:40:15 +0530

    .cpp

---
+++ object oriented/functionInClass.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class person{
+    public:
+    string name;
+    int runs;
+  float avg ;
+     person(string name,int runs,float avg){
+       this->name=name;                  //prints garbage values 
+       this->runs=runs;
+       this->avg=avg;
+     }
+
+       void print(){           //function inside class
+        cout<<name<<" "<<runs  << " "<<avg<<endl;
+    }
+ 
+      
+};
+
+
+int main(){
+    person c1("wyrat kohli ",25000,56.06);
+   
+    person c2("Brohit sharma ",18000,55.5);
+
+   c1.print();
+  c2.print();
+    return 0;
+}
--- object oriented/thisKeyword.cpp
+++ object oriented/thisKeyword.cpp
@@ -6,20 +6,16 @@ class person{
     string name;
     int runs;
   
-    // person(string name,int runs){
-    //     name=name;                  //prints garbage values 
-    //     runs=runs;
-    // }
-      person(string n,int r){
-        name=n;
-        runs = r;
-        
-      }
+     person(string name,int runs){
+       this->name=name;                  //prints garbage values 
+       this->runs=runs;
+ }
+      
 };
 int main(){
     person c1("wyrat kohli ",25000);
    
-    person c2("Brohit sharma",18000);
+    person c2("Brohit sharma ",18000);
 
     cout<<c1.name<<c1.runs<<endl;
     cout<<c2.name<<c2.runs<<endl;
