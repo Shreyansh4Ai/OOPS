8fb58d55c63ced89d43aea53bfc4b6553bba1748
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-04-20 17:57:50 +0530

    Create tyre.cpp

---
+++ conditional statements/tyre.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin >> t ; 
+while ( t-- ){
+    int n,m;
+    cin >> n >> m ; 
+    int total = n*2 + m*4;
+    cout << total << endl ;
+    
+}
+return 0;
+
+}
