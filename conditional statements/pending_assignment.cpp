2adcfb24f9345f77faeeecaa672fa94414b9d4a1
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-04-22 23:18:56 +0530

    Create pending_assignment.cpp

---
+++ conditional statements/pending_assignment.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ; 
+	cin >> t ; 
+	while (t--){
+	    
+	
+int x , y ,z ;
+cin >> x >> y >> z ;
+int req= x*y;
+int min = z*24*60 ; 
+if (req<=min) cout <<"YES"<<endl;
+else cout <<"NO"<<endl;
+}
+return 0;
+}
