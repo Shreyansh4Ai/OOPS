6f740275ec9af5bd5b4cdcbddb8e637cde266094
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-04-23 18:20:56 +0530

    Create maxTest.cpp

---
+++ conditional statements/maxTest.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t ; 
+cin >> t;
+while ( t--){
+  int n , m ,k ; 
+  cin >> n >> m >> k ; 
+  if ( n+k <= m) cout <<"YES"<<endl;
+  else cout <<"NO"<<endl;
+    
+}
+return 0;
+}
