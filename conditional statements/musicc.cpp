fb61c48999af2d1b1eada6f005af44b5a73f44ed
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-04-27 15:54:39 +0530

    Create musicc.cpp

---
+++ conditional statements/musicc.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t ;
+cin >> t ;
+while (t--){
+    int n , x;
+    cin >> n >> x ;
+    int c = n / (x*3);
+    cout << c <<endl;
+}
+return 0;
+}
