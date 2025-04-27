6514d80487ee1c068578654d37dbb863f8d58657
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-04-27 16:37:02 +0530

    Create discount.cpp

---
+++ conditional statements/discount.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t ;
+cin >> t ;
+while(t--){
+   int x; 
+   cin >>x ;
+   int d1=x/10;
+   int d2 = 100;
+   cout << max(d1,d2) << endl;
+}
+return 0;
+}
