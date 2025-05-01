bfcab29cade6d9b6b3b8c42c1351a3e4b8353ccb
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-05-01 15:13:59 +0530

    Create bathing.cpp

---
+++ conditional statements/bathing.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t ;
+cin >> t ;
+while (t--){
+    int x , y ;
+    cin>> x >> y ;
+    int ltr=2*y;
+    int people = x/ltr;
+    cout << people << endl;
+}
+return 0;
+}
